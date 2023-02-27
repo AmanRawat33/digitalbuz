f92a9d6b6f04c0e4e48a3cf22453685c86c7cc03
AmanRawat33 <96495347+AmanRawat33@users.noreply.github.com>
2023-02-27 19:43:15 +0530

    Add files

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+window.onload = function digital_fn() {
+
+    let toggle = document.querySelector("#nav .toggle-btn");
+    let collapse = document.querySelector("#nav .collapse");
+
+    toggle.addEventListener("click", function (event) {
+        collapse.classList.toggle('active');
+     });
+}
